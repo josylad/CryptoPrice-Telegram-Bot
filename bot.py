b2ae3a970ca029faa065b20a397e6e2137d9a6db
Joseph Adediji <josylado@gmail.com>
2021-03-22 11:30:43 +0100

    fix bug

--- bot.py
+++ bot.py
@@ -43,9 +43,26 @@ def price_finder(message):
 			bot.reply_to(message, response)
 
 
+	if message.chat.type == "private":
+		try:
+			query = message.text.split()
+			coin_symbol = query[1].upper()
+			print(coin_symbol)
+			crypto_requests = requests.get("https://wavescap.com/api/asset/{}.json".format(coin_symbol))
+			crypto_data = crypto_requests.json()
+			coin_price = crypto_data["data"]["lastPrice_usd-n"]
+			coin_name = crypto_data["name"]
+			response = '{} - {} \n ${} \n'.format(coin_symbol,coin_name,coin_price,)
+			bot.reply_to(message, response)
+		
+		except Exception as e:
+			print(e)
+			response = "Sorry, We do not support this Token/Coin at this Time\n\n Enter '/price Coin Symbol' or Shortcode to get started\n E.g, '/price BTC', ETH, NSBT"
+			bot.reply_to(message, response)
+
+
 
 # this handles messages in private chats with the bot 
-@bot.message_handler(commands=['price', 'p', 'waves'])
 @bot.message_handler(regexp='')
 def price_finder(message):
 	if message.chat.type == "private":
